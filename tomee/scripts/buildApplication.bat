b245e8b133b9a8eac5b4367b328a827a139934d5
witchpou <witchpou@7a66ed79-f1f7-4962-a21a-66b68da613ba>
2014-03-23 18:06:23 +0000

    add scripts in eclipse

--- tomee/scripts/buildApplication.bat
+++ tomee/scripts/buildApplication.bat
@@ -3,4 +3,4 @@ FOR /F "tokens=1,2 delims==" %%G IN (env.properties) DO (set %%G=%%H)
 echo Project Dir: %projectDir% , Application: %applicationName%
 cd %projectDir%\%applicationName%
 call mvn clean install
-pause
+REM pause
--- tomee/scripts/redeployApplication.bat
+++ tomee/scripts/redeployApplication.bat
@@ -1,8 +1,8 @@
 @echo off
 FOR /F "tokens=1,2 delims==" %%G IN (env.properties) DO (set %%G=%%H)
-echo delete %webApp%\%tomeeWebappDir% and %webApp%\%tomeeWebappDir%.war
-del %tomeeWebappDir%\%applicationName%
-del %tomeeWebappDir%\%warName%
+echo delete %tomeeWebappDir%\%applicationName% and %tomeeWebappDir%\%warName%
+rmdir /s /q %tomeeWebappDir%\%applicationName%
+del /F/Q %tomeeWebappDir%\%warName%
 echo copy %projectDir%\%applicationName%\%warDir%\%warName% to %tomeeWebappDir%\
 copy %projectDir%\%applicationName%\%warDir%\%warName% %tomeeWebappDir%
-pause
+REM pause
