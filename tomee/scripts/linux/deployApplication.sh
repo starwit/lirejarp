c1fce15f729516c9bf47537578d36d726ff727a6
kronprinz <kronprinz@7a66ed79-f1f7-4962-a21a-66b68da613ba>
2014-10-02 14:33:53 +0000

    deploy script for jenkins remove tomee start

--- tomee/scripts/linux/deployApplication.sh
+++ tomee/scripts/linux/deployApplication.sh
@@ -4,11 +4,6 @@ INSTANCE_PATH=$1 # where runs tomee
 PACKAGE_PATH=$2 # where to find stuff to deploy
 DEPLOY_PATH=$INSTANCE_PATH/webapps
 
-# stop tomee
-$INSTANCE_PATH/bin/shutdown.sh
-
-sleep 6 # wait some time to let tomee finish shutdown
-
 # delete webapps/war file
 rm -f $INSTANCE_PATH/webapps/lirejarp.war
 rm -rf $INSTANCE_PATH/webapps/lirejarp
@@ -17,4 +12,4 @@ rm -rf $INSTANCE_PATH/webapps/lirejarp
 cp $PACKAGE_PATH $DEPLOY_PATH
 
 # start tomee
-nohup $INSTANCE_PATH/bin/startup.sh &> $INSTANCE_PATH/logs/nohup.log
+#nohup $INSTANCE_PATH/bin/startup.sh &> $INSTANCE_PATH/logs/nohup.log
