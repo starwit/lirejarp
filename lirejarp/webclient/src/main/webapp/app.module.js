b850425a8b44c60a5aa74930f9547c7ce792f189
witchpou <witchpou@gmx.de>
2016-10-11 11:47:31 +0200

    rename rest project (prior business) adopted to new fremarker templates

--- lirejarp/webclient/src/main/webapp/app.module.js
+++ lirejarp/webclient/src/main/webapp/app.module.js
@@ -1,21 +1,36 @@
-'use strict';
-
-// Declare app level module which depends on views, and components
-var lirejarpApp = angular.module('lirejarpApp', [
-    'pascalprecht.translate',
-    //###BEGIN### include generated files
-	//###END### include generated files
-	'ngRoute'
-]);
-
-lirejarpApp.config(['$translateProvider', function($translateProvider) {
-	$translateProvider
-	.useStaticFilesLoader({
-		prefix: 'localization/translations-',
-		suffix: '.json'
-	}).preferredLanguage('de-DE')
-		.useSanitizeValueStrategy('escaped') // Security for escaping variables
-		.usePostCompiling(true); // Post compiling angular filters
-}]);
+(function() {
+	'use strict';
+	// Declare app level module which depends on views, and components
+	var lirejarpApp = angular.module('lirejarpApp', [
+	    'pascalprecht.translate',
+	    //###BEGIN### include generated files
+		//###END### include generated files
+		'ngRoute'
+	]);
 	
-lirejarpApp.factory('restConnectorFactory', restConnectorFactory);
+	angular.module('lirejarpApp').config(['$translateProvider', function($translateProvider) {
+		$translateProvider
+		.useStaticFilesLoader({
+			prefix: 'localization/translations-',
+			suffix: '.json'
+		}).preferredLanguage('de-DE')
+			.useSanitizeValueStrategy('escaped') // Security for escaping variables
+			.usePostCompiling(true); // Post compiling angular filters
+	}]);
+	
+	angular.module('lirejarpApp').controller('appController', appController);
+	
+	/**
+	 * Controller for global behavior when changing the view (routeChange).
+	 * @param $scope
+	 * @returns
+	 */
+	function appController($scope) {
+		$scope.$on('$routeChangeSuccess', function (scope, next, current) {
+			$scope.title=next.title;
+			$scope.subtitle=next.subtitle;
+		});
+
+			
+	angular.module('lirejarpApp').factory('restConnectorFactory', restConnectorFactory);
+})();
--- lirejarp/webclient/src/main/webapp/app.routes.js
+++ lirejarp/webclient/src/main/webapp/app.routes.js
@@ -1,3 +1,6 @@
-lirejarpApp.config(['$routeProvider', function($routeProvider) {
+/** 
+ * Navigation and routing for module ljprojectbuilderApp.
+ */
+angular.module('lirejarpApp').config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/'});
 }]);
